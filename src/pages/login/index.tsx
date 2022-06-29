3f3b8ca9b9d5a2f38f88a3b43c2f1a62b726fea0
jayasree96k <jayasree.karunanithi@kprinfo.com>
2022-06-29 15:24:51 +0530

    login page color changes

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,17 +1,35 @@
-import * as React from 'react'
-import { useLocation, useNavigate } from 'react-router-dom'
-import Avatar from '@mui/material/Avatar'
-import Button from '@mui/material/Button'
-import TextField from '@mui/material/TextField'
-import FormControlLabel from '@mui/material/FormControlLabel'
-import Checkbox from '@mui/material/Checkbox'
-import Link from '@mui/material/Link'
-import Grid from '@mui/material/Grid'
-import Box from '@mui/material/Box'
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined'
-import Typography from '@mui/material/Typography'
-import Container from '@mui/material/Container'
-import { setLocalStorageItem } from '../../services/localStorage'
+import * as React from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import styled from "@emotion/styled";
+import { setLocalStorageItem } from "../../services/localStorage";
+
+const CssTextField = styled(TextField)({
+  "& label.Mui-focused": {
+    color: "#181C32",
+  },
+  "& .MuiInput-underline:after": {
+    borderBottomColor: "green",
+  },
+  "& .MuiOutlinedInput-root": {
+    "&:hover fieldset": {
+      borderColor: "#181C32",
+    },
+    "&.Mui-focused fieldset": {
+      borderColor: "#181C32",
+    },
+  },
+});
 
 function Copyright(props: any) {
   return (
@@ -21,48 +39,53 @@ function Copyright(props: any) {
       align="center"
       {...props}
     >
-      {'Copyright © '}
+      {"Copyright © "}
       <Link color="inherit" href="https://mui.com/">
         Your Website
-      </Link>{' '}
+      </Link>{" "}
       {new Date().getFullYear()}
     </Typography>
-  )
+  );
 }
 
 export default function SignIn() {
-  let navigate = useNavigate()
+  let navigate = useNavigate();
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault()
-    const data = new FormData(event.currentTarget)
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
     console.log({
-      email: data.get('email'),
-      password: data.get('password'),
-    })
-    setLocalStorageItem('authToken', 'test').then(() => {
-      navigate('/')
-    })
-  }
+      email: data.get("email"),
+      password: data.get("password"),
+    });
+    setLocalStorageItem("authToken", "test").then(() => {
+      navigate("/");
+    });
+  };
 
   return (
     <Container component="main" maxWidth="xs">
       <Box
         sx={{
           marginTop: 8,
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
         }}
       >
-        <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+        <Avatar sx={{ m: 1, bgcolor: "#181C32" }}>
           <LockOutlinedIcon />
         </Avatar>
         <Typography component="h1" variant="h5">
           Sign in
         </Typography>
-        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-          <TextField
+        <Box
+          component="form"
+          onSubmit={handleSubmit}
+          noValidate
+          sx={{ mt: 1, color: "#181C32" }}
+        >
+          <CssTextField
             margin="normal"
             required
             fullWidth
@@ -72,7 +95,29 @@ export default function SignIn() {
             autoComplete="email"
             autoFocus
           />
-          <TextField
+          {/* <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus 
+
+            // sx={{
+            //   "& .MuiInputLabel-root": { color: "#181C32" }, //styles the label
+            //   "& .MuiOutlinedInput-root": {
+            //     "& > fieldset": { borderColor: "#181C32" },
+            //   },
+            //   "& .MuiOutlinedInput-root.Mui-focused": {
+            //     "& > fieldset": {
+            //       borderColor: "#181C32",
+            //     },
+            //   },
+            // }}
+      /> */}
+          <CssTextField
             margin="normal"
             required
             fullWidth
@@ -83,25 +128,46 @@ export default function SignIn() {
             autoComplete="current-password"
           />
           <FormControlLabel
-            control={<Checkbox value="remember" color="primary" />}
+            control={
+              <Checkbox
+                value="remember"
+                // color="primary"
+                sx={{
+                  "&.Mui-checked ": {
+                    color: "#181C32",
+                  },
+                }}
+              />
+            }
             label="Remember me"
           />
           <Button
             type="submit"
             fullWidth
             variant="contained"
-            sx={{ mt: 3, mb: 2 }}
+            color="inherit"
+            sx={{
+              mt: 3,
+              mb: 2,
+              "&.MuiButton-colorInherit": {
+                backgroundColor: "#212e73",
+                color: "white",
+              },
+              "&:hover": {
+                backgroundColor: "#181C32",
+              },
+            }}
           >
             Sign In
           </Button>
           <Grid container>
             <Grid item xs>
-              <Link href="#" variant="body2">
+              <Link href="#" variant="body1" sx={{ color: "#181C32" }}>
                 Forgot password?
               </Link>
             </Grid>
             <Grid item>
-              <Link href="#" variant="body2">
+              <Link href="#" variant="body1" sx={{ color: "#181C32" }}>
                 Don't have an account? Sign Up
               </Link>
             </Grid>
@@ -110,5 +176,5 @@ export default function SignIn() {
       </Box>
       <Copyright sx={{ mt: 8, mb: 4 }} />
     </Container>
-  )
+  );
 }
