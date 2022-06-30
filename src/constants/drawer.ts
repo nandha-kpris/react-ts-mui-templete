f0b64bbd897ec86a37b432a3fa31d88a243df065
pavithrarajkumar5287 <104818006+pavithrarajkumar5287@users.noreply.github.com>
2022-06-30 15:32:22 +0530

    Line chart

--- src/constants/drawer.ts
+++ src/constants/drawer.ts
@@ -1,16 +1,12 @@
-import { IdrawerOptions } from "../components/CustomDrawer";
+import { IdrawerOptions } from '../components/CustomDrawer';
 
 export const drawerOptions: IdrawerOptions[] = [
   {
-    label: "Home",
-    path: "/pages/home",
-  },
-  {
-    label: "Table",
-    path: "/pages/table",
-  },
-  {
     label: 'DashBoard',
     path: '/pages/dashBoard',
+  },
+  {
+    label: 'Table',
+    path: '/pages/table',
   },
 ];
